9f695e41741c7999b8b4d71bd73b5fb87b973eca
Benjamin <benjamin.goisser.bg@gmail.com>
2020-02-19 17:22:55 +0100

    Completed WiFi settings page

--- src/makrotouch/settings/SettingsActions.java
+++ src/makrotouch/settings/SettingsActions.java
@@ -11,58 +11,69 @@ import java.io.InputStreamReader;
 import static javax.swing.JOptionPane.showMessageDialog;
 
 public class SettingsActions implements ActionListener {
-
+	
 	private boolean connected = false;
-
+	
 	@Override
 	public void actionPerformed(ActionEvent actionEvent) {
 		SettingsManager settings = Main.getSettings();
-
-		if (actionEvent.getSource() == settings.getBtCancel()) {
+		
+		if(actionEvent.getSource() == settings.getBtCancel()) {
 			settings.getWindow().dispose();
-		} else if (actionEvent.getSource() == settings.getBtConnect()) {
+			System.out.println("Cancel");
+		} else if(actionEvent.getSource() == settings.getBtConnect()) {
+			if(settings.getLiWifi().getSelectedIndex() == -1) {
+				System.out.println("Connect triggered, no network selected");
+				showMessageDialog(settings.getWindow(), "Please select a WiFi network from the list!");
+				return;
+			}
+			
 			try {
-				if (Main.isRelease()) {
+				if(Main.isRelease()) {
 					Runtime.getRuntime().exec("nmcli dev disconnect iface wlan0");
 				} else {
 					Runtime.getRuntime().exec("nmcli dev disconnect iface wlp3s0");
 				}
 				Thread.sleep(1000);
-
+				
 				String ssid = (String) settings.getLiWifi().getSelectedValue();
 				Runtime.getRuntime().exec("nmcli dev wifi connect " + ssid +
 						" password " + settings.getTfPassword().getText(), null);
-
+				
 				int i = 0;
-				while (!connected) {
+				while(!connected) {
 					Process checkConnected = Runtime.getRuntime().exec("nmcli -f in-use dev wifi", null);
 					BufferedReader reader = new BufferedReader(new InputStreamReader(checkConnected.getInputStream()));
-
+					
 					Thread.sleep(500);
-
+					
 					String currentLine;
-					while ((currentLine = reader.readLine()) != null) {
-						if (currentLine.contains("*")) {
+					while((currentLine = reader.readLine()) != null) {
+						if(currentLine.contains("*")) {
 							connected = true;
 						}
 					}
 					i++;
-					if (i >= 20) {
+					if(i >= 20) {
 						break;
 					}
 				}
-
-				if (connected) {
+				
+				if(connected) {
 					showMessageDialog(settings.getWindow(), "Connected!");
 					settings.getWindow().dispose();
 				} else {
 					showMessageDialog(settings.getWindow(), "Could not connect to wifi network!");
 				}
-
-			} catch (IOException | InterruptedException e) {
+				
+			} catch(IOException | InterruptedException e) {
 				System.out.println("Couldn't connect to WiFi network");
 				showMessageDialog(settings.getWindow(), "Couldn't connect to WiFi network!");
 			}
+		} else if(actionEvent.getSource() == settings.getBtDown()) {
+			settings.scrollDown();
+		} else if(actionEvent.getSource() == settings.getBtUp()) {
+			settings.scrollUp();
 		}
 	}
 }
--- src/makrotouch/settings/SettingsManager.java
+++ src/makrotouch/settings/SettingsManager.java
@@ -4,91 +4,88 @@ import makrotouch.display.Window;
 import makrotouch.main.Main;
 
 import javax.swing.*;
-import javax.swing.border.Border;
-import javax.swing.border.CompoundBorder;
-import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class SettingsManager {
-
-    private JFrame window;
-    private ActionListener listener = new SettingsActions();
-    private OnScreenKeyboardLauncher focusListener = new OnScreenKeyboardLauncher();
-    private Dimension screenBounds = Toolkit.getDefaultToolkit().getScreenSize();
-
-    private JButton btConnect, btCancel, btUp, btDown;
-    private JList liWifi;
-    private JLabel lbLoading;
-    private JPasswordField tfPassword;
-
-    private ArrayList<String> wifiNetworks;
-    private String[] wifiNetworksFinal;
-    private boolean open = true;
-
-    public SettingsManager() {
-        if (Main.isRelease()) {
-            window = new Window("Settings", (int) screenBounds.getWidth(), (int) screenBounds.getHeight(), false, false, true);
-        } else {
-            window = new Window("Settings", 1024, 600, false, true, true);
-        }
-
-        window.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                open = false;
-            }
-
-            @Override
-            public void windowClosed(WindowEvent e) {
-                open = false;
-            }
-        });
-
-        wifiNetworks = new ArrayList<>();
-
-        lbLoading = new JLabel("Loading WiFi Networks...", JLabel.CENTER);
-
-        window.getContentPane().setBackground(Color.black);
-
-        initLoading();
-        initNetworks();
-    }
-
-    public boolean isOpen() {
-        return open;
-    }
-
-    public void setOpen(boolean open) {
-        this.open = open;
-    }
-
-    private void initLoading() {
-        lbLoading.setPreferredSize(new Dimension((int) screenBounds.getWidth(), 40));
-        lbLoading.setMaximumSize(new Dimension((int) screenBounds.getWidth(), 40));
-        lbLoading.setMinimumSize(new Dimension((int) screenBounds.getWidth(), 40));
-        lbLoading.setForeground(Color.WHITE);
-        window.getContentPane().add(lbLoading);
-        window.pack();
-        lbLoading.setVisible(true);
-    }
-
-    public JTextField getTfPassword() {
-        return tfPassword;
-    }
-
-    private void initNetworks() {
-        new Thread(() -> {
+	
+	private final int listSize = 22;
+	
+	private JFrame window;
+	private ActionListener listener = new SettingsActions();
+	private OnScreenKeyboardLauncher focusListener = new OnScreenKeyboardLauncher();
+	private Dimension screenBounds = Toolkit.getDefaultToolkit().getScreenSize();
+	
+	private JButton btConnect, btCancel, btUp, btDown;
+	private JList liWifi;
+	private JLabel lbLoading;
+	private JPasswordField tfPassword;
+	
+	private ArrayList<String> wifiNetworks;
+	private DefaultListModel<String> wifiNetworksVisible;
+	private boolean open = true;
+	private int scrollIndex = 0;
+	
+	public SettingsManager() {
+		if(Main.isRelease()) {
+			window = new Window("Settings", (int) screenBounds.getWidth(), (int) screenBounds.getHeight(), false, false, true);
+		} else {
+			window = new Window("Settings", 1024, 600, false, true, true);
+		}
+		
+		window.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				open = false;
+			}
+			
+			@Override
+			public void windowClosed(WindowEvent e) {
+				open = false;
+			}
+		});
+		
+		wifiNetworks = new ArrayList<>();
+		wifiNetworksVisible = new DefaultListModel<>();
+		
+		lbLoading = new JLabel("Loading WiFi Networks...", JLabel.CENTER);
+		
+		window.getContentPane().setBackground(Color.black);
+		
+		initLoading();
+		initNetworks();
+	}
+	
+	private void initLoading() {
+		lbLoading.setPreferredSize(new Dimension((int) screenBounds.getWidth(), 40));
+		lbLoading.setMaximumSize(new Dimension((int) screenBounds.getWidth(), 40));
+		lbLoading.setMinimumSize(new Dimension((int) screenBounds.getWidth(), 40));
+		lbLoading.setForeground(Color.WHITE);
+		window.getContentPane().add(lbLoading);
+		window.pack();
+		lbLoading.setVisible(true);
+	}
+	
+	private void initNetworks() {
+		new Thread(() -> {
+			for(int i = 0; i < 60; i++) {
+				wifiNetworks.add("Wifi Network " + i);
+			}
+			
+			wifiNetworksVisible.clear();
+			
+			for(int i = scrollIndex; i < listSize + scrollIndex; i++) {
+				wifiNetworksVisible.add(i, wifiNetworks.get(i));
+			}
+            /*
             try {
+                
                 System.out.print("Fetching WiFi networks...");
                 Process wifiCommand = Runtime.getRuntime().exec("nmcli -f ssid dev wifi", null);
-
+                
                 BufferedReader commandOutputReader = new BufferedReader(new InputStreamReader(wifiCommand.getInputStream()));
                 String currentLine;
                 int i = 0;
@@ -104,105 +101,150 @@ public class SettingsManager {
                 wifiNetworks.forEach(System.out::println);
 
                 wifiCommand.destroy();
-
-                initElem();
-
-            } catch (IOException e) {
+                
+                 */
+			
+			initElem();
+           
+           /* } catch (IOException e) {
                 javax.swing.JOptionPane.showMessageDialog(this.getWindow(), "Couldn't fetch wifi networks!");
                 window.dispose();
-            }
-        }).start();
-
-    }
-
-    public JButton getBtConnect() {
-        return btConnect;
-    }
-
-    public JFrame getWindow() {
-        return window;
-    }
-
-    public JButton getBtCancel() {
-        return btCancel;
-    }
-
-    public JButton getBtUp() {
-        return btUp;
-    }
-
-    public JButton getBtDown() {
-        return btDown;
-    }
-
-    public JList getLiWifi() {
-        return liWifi;
-    }
-
-    private void initElem() {
-
-        lbLoading.setVisible(false);
-        window.getContentPane().remove(lbLoading);
-
-        btUp = new JButton("UP");
-        btUp.setPreferredSize(new Dimension(800, 20));
-        btUp.setMaximumSize(new Dimension(800, 20));
-        btUp.setMinimumSize(new Dimension(800, 20));
-        btUp.setBackground(Color.lightGray);
-        btUp.setAlignmentX(Component.CENTER_ALIGNMENT);
-        window.getContentPane().add(btUp);
-        window.pack();
-        btUp.setVisible(true);
-
-        liWifi = new JList(wifiNetworks.toArray());
-        liWifi.setPreferredSize(new Dimension(800, 400));
-        liWifi.setMaximumSize(new Dimension(800, 400));
-        liWifi.setMinimumSize(new Dimension(800, 400));
-        liWifi.setAlignmentX(Component.CENTER_ALIGNMENT);
-        window.getContentPane().add(liWifi);
-        window.pack();
-        liWifi.setVisible(true);
-
-        btDown = new JButton("DOWN");
-        btDown.setPreferredSize(new Dimension(800, 20));
-        btDown.setMaximumSize(new Dimension(800, 20));
-        btDown.setMinimumSize(new Dimension(800, 20));
-        btDown.setBackground(Color.lightGray);
-        btDown.setAlignmentX(Component.CENTER_ALIGNMENT);
-        window.getContentPane().add(btDown);
-        window.pack();
-        btDown.setVisible(true);
-
-        tfPassword = new JPasswordField();
-        tfPassword.setPreferredSize(new Dimension(600, 20));
-        tfPassword.setMaximumSize(new Dimension(600, 20));
-        tfPassword.setMinimumSize(new Dimension(600, 20));
-        tfPassword.setAlignmentX(Component.CENTER_ALIGNMENT);
-        tfPassword.addFocusListener(focusListener);
-        window.getContentPane().add(tfPassword);
-        window.pack();
-        tfPassword.setVisible(true);
-
-        btConnect = new JButton("Connect");
-        btConnect.setPreferredSize(new Dimension(200, 50));
-        btConnect.setMaximumSize(new Dimension(200, 50));
-        btConnect.setMinimumSize(new Dimension(200, 50));
-        btConnect.setBackground(Color.GREEN);
-        btConnect.setAlignmentX(Component.CENTER_ALIGNMENT);
-        btConnect.addActionListener(listener);
-        window.getContentPane().add(btConnect);
-        window.pack();
-        btConnect.setVisible(true);
-
-        btCancel = new JButton("Cancel");
-        btCancel.setPreferredSize(new Dimension(200, 50));
-        btCancel.setMaximumSize(new Dimension(200, 50));
-        btCancel.setMinimumSize(new Dimension(200, 50));
-        btCancel.setBackground(Color.RED);
-        btCancel.addActionListener(listener);
-        btCancel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        window.getContentPane().add(btCancel);
-        window.pack();
-        btCancel.setVisible(true);
-    }
+            } */
+		}).start();
+		
+	}
+	
+	private void initElem() {
+		
+		lbLoading.setVisible(false);
+		window.getContentPane().remove(lbLoading);
+		
+		btUp = new JButton("UP");
+		btUp.setPreferredSize(new Dimension(800, 20));
+		btUp.setMaximumSize(new Dimension(800, 20));
+		btUp.setMinimumSize(new Dimension(800, 20));
+		btUp.setBackground(Color.lightGray);
+		btUp.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btUp.addActionListener(listener);
+		window.getContentPane().add(btUp);
+		window.pack();
+		btUp.setVisible(true);
+		
+		liWifi = new JList(wifiNetworks.toArray());
+		liWifi.setPreferredSize(new Dimension(800, 400));
+		liWifi.setMaximumSize(new Dimension(800, 400));
+		liWifi.setMinimumSize(new Dimension(800, 400));
+		liWifi.setAlignmentX(Component.CENTER_ALIGNMENT);
+		window.getContentPane().add(liWifi);
+		window.pack();
+		liWifi.setVisible(true);
+		
+		btDown = new JButton("DOWN");
+		btDown.setPreferredSize(new Dimension(800, 20));
+		btDown.setMaximumSize(new Dimension(800, 20));
+		btDown.setMinimumSize(new Dimension(800, 20));
+		btDown.setBackground(Color.lightGray);
+		btDown.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btDown.addActionListener(listener);
+		window.getContentPane().add(btDown);
+		window.pack();
+		btDown.setVisible(true);
+		
+		tfPassword = new JPasswordField();
+		tfPassword.setPreferredSize(new Dimension(600, 20));
+		tfPassword.setMaximumSize(new Dimension(600, 20));
+		tfPassword.setMinimumSize(new Dimension(600, 20));
+		tfPassword.setAlignmentX(Component.CENTER_ALIGNMENT);
+		tfPassword.addFocusListener(focusListener);
+		window.getContentPane().add(tfPassword);
+		window.pack();
+		tfPassword.setVisible(true);
+		
+		btConnect = new JButton("Connect");
+		btConnect.setPreferredSize(new Dimension(200, 50));
+		btConnect.setMaximumSize(new Dimension(200, 50));
+		btConnect.setMinimumSize(new Dimension(200, 50));
+		btConnect.setBackground(Color.GREEN);
+		btConnect.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btConnect.addActionListener(listener);
+		window.getContentPane().add(btConnect);
+		window.pack();
+		btConnect.setVisible(true);
+		
+		btCancel = new JButton("Cancel");
+		btCancel.setPreferredSize(new Dimension(200, 50));
+		btCancel.setMaximumSize(new Dimension(200, 50));
+		btCancel.setMinimumSize(new Dimension(200, 50));
+		btCancel.setBackground(Color.RED);
+		btCancel.addActionListener(listener);
+		btCancel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		window.getContentPane().add(btCancel);
+		window.pack();
+		btCancel.setVisible(true);
+	}
+	
+	public void scrollDown() {
+		if(!(scrollIndex >= wifiNetworks.size() - listSize)) {
+			wifiNetworksVisible.remove(0);
+			wifiNetworksVisible.addElement(wifiNetworks.get(scrollIndex + listSize));
+			liWifi.removeAll();
+			liWifi.setModel(wifiNetworksVisible);
+			scrollIndex++;
+		}
+	}
+	
+	public void scrollUp() {
+		if(scrollIndex != 0) {
+		    wifiNetworksVisible.remove(listSize - 1);
+		    wifiNetworksVisible.insertElementAt(wifiNetworks.get(scrollIndex - 1), 0);
+		    liWifi.removeAll();
+		    liWifi.setModel(wifiNetworksVisible);
+		    scrollIndex--;
+		}
+	}
+	
+	public int getScrollIndex() {
+		return scrollIndex;
+	}
+	
+	public int getListSize() {
+		return listSize;
+	}
+	
+	
+	public boolean isOpen() {
+		return open;
+	}
+	
+	public void setOpen(boolean open) {
+		this.open = open;
+	}
+	
+	public JTextField getTfPassword() {
+		return tfPassword;
+	}
+	
+	public JButton getBtConnect() {
+		return btConnect;
+	}
+	
+	public JFrame getWindow() {
+		return window;
+	}
+	
+	public JButton getBtCancel() {
+		return btCancel;
+	}
+	
+	public JButton getBtUp() {
+		return btUp;
+	}
+	
+	public JButton getBtDown() {
+		return btDown;
+	}
+	
+	public JList getLiWifi() {
+		return liWifi;
+	}
 }
